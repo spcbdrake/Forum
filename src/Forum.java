8bf9eefc7c890a9465fa3c92aa70a6b29d1cf468
benjamindrake <spcbdrake@yahoo.com>
2015-10-15 11:33:57 -0400

    add file methods and post.txt

---
+++ src/Forum.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ * Created by benjamindrake on 10/15/15.
+ */
+public class Forum {
+    public static void main(String[] args) {
+    }
+
+    static String readFile(String fileName) {
+        File f = new File(fileName);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] fileContent = new char[fileSize];
+            fr.read(fileContent):
+            return new String(fileContent);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    static void writeFile(String fileName, String fileContent) {
+        File f = new File(fileName);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(fileContent);
+            fw.close();
+        } catch (Exception e) {
+
+        }
+    }
+}
